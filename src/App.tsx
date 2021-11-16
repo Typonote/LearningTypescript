fc21125f8c2c1f657c2dce2eeef76e5ab42efacd
JinKyung Kim <kimradium@gmail.com>
2021-11-16 13:36:41 +0900

    #6 지도 test

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import "./_App.scss";
 import Member from "./Routes/Member";
 import Dictionary from "./Routes/Dictionary";
+import Map from "./Component/Common/Map";
+import Dummy4 from "./Sample/Dummy4";
 
 function App() {
   return (
     <div className="App__Container">
       {/* <Member /> */}
-      <Dictionary />
+      {/* <Dictionary /> */}
+      <Map data={Dummy4} />
     </div>
   );
 }
