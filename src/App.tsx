1bc511bcfea64d9ec9d743d4af5978b001125718
JinKyung Kim <kimradium@naver.com>
2021-11-18 19:51:22 +0900

    #12 업로드 기능 테스트

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,15 @@ import Member from "./Routes/Member";
 import Dictionary from "./Routes/Dictionary";
 import Map from "./Component/Common/Map";
 import Location from "./Sample/Location";
+import Analysis from "./Routes/Analysis";
 
 function App() {
   return (
     <div className="App__Container">
       {/* <Member /> */}
-      <Dictionary />
+      {/* <Dictionary /> */}
       {/* <Map data={Location} /> */}
+      <Analysis />
     </div>
   );
 }
---
+++ src/Routes/Analysis.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Upload from "../Component/Analysis/Upload";
+
+const Analysis = () => {
+  return (
+    <div>
+      <Upload />
+    </div>
+  );
+};
+
+export default Analysis;
